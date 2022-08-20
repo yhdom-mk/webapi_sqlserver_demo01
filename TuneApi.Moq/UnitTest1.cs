a3c563e48a544b25a41f3a0117c359a2249c023f
Mski Ydom <lcydome.msk12@gmail.com>
2022-08-20 20:23:16 +0900

    単体テスト 実装

--- TuneApi.Moq/UnitTest1.cs
+++ TuneApi.Moq/UnitTest1.cs
@@ -2,12 +2,33 @@ using Xunit;
 
 namespace TuneApi.Moq
 {
-    public class UnitTest1
+    public class TuneControllerTest
     {
-        [Fact]
-        public void Test1()
-        {
+        //private readonly TuneController _controller;
+        //private readonly
 
+        public TuneControllerTest()
+        {
+            //_controller = new TuneController();
+        }
+        [Fact]
+
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            //Act
+            //var okResult = _controller.Get();
+
+            //Assert
+            //Assert.IsType<OkObjectResult>(okResult as OkObjectResult);
+        }
+        public void Get_WhenCalled_ReturnsAllTunes() 
+        {
+            //Act
+            //var okResult = _controller.Get() as OkObjectResult;
+
+            //Assert
+            //var tune = Assert.IsType<List<OkObjectResult>>(okResult.Value);
+            //Assert.Equal(1, tune.Count);
         }
     }
 }
